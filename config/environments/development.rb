d6c6acb3017944d1a36ae5facc29c1a52bb6dc1d
Nicholas Hebert <nick@devnick.com>
2017-11-08 12:46:11 -0600

    added support for mailcatcher Docker container

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { host: 'mailcatcher' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'mailcatcher',
+    port:           1025,
+    domain:         'localhost'
+  }
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
