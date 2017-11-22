a82fc4b9ecc0e1d5a16d6fb93b7b914157ba927b
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-11-21 18:47:32 -0600

    stripe issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :wikis
 
+  resources :charges, only: [:new, :create]
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
