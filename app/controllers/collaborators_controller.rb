8cab6f6a9635be763fd32eb36eee8921018ec042
Nicholas Hebert <nick@devnick.com>
2017-11-29 11:43:36 -0600

    revised tables to allow for non-nested forms for adding/removing collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,5 +1,5 @@
 class CollaboratorsController < ApplicationController
-  before_action :set_wiki, only: [:index, :create]
+  before_action :set_wiki, only: [:index, :create, :destroy]
 
   def index
     @users = User.all
@@ -17,27 +17,22 @@ class CollaboratorsController < ApplicationController
   end
 
   def create
-    puts params[:wiki][:collaborators]
-    # @wiki.collaborators = {'user_id' => []}.merge(params[:collaborators] || {})
-    #@wiki.users << User.where(id: params[:wiki][:collaborators])
-    params[:wiki][:collaborators].each do |user_id|
-      # Create a new Collaborator record with @wiki.id and user_id
-      Collaborator.find_or_create_by(
-        wiki_id: @wiki.id,
-        user_id: user_id
-      )
-    end
+    collaborator = Collaborator.new(wiki_id: @wiki.id, user_id: params[:user_id])
 
-    @wiki.save!
+    if collaborator.save
+      flash[:success] = "Collaborator added to wiki."
+      redirect_to wiki_collaborators_path(@wiki)
+    else
+      flash[:error] = "There was an error deleting this collaborator. Please try again."
+    end
   end
 
   def destroy
-    @wiki = Wiki.find(params[:wiki_id])
-    @collaborator = Collaborator.new(user_id: params[:user_id], wiki_id: params[:wiki_id])
+    collaborator = Collaborator.find_by(user_id: params[:user_id], wiki_id: @wiki.id)
 
-    if @collaborator.destroy
-      flash[:notice] = "Collaborator removed from wiki."
-      redirect_to @wiki
+    if collaborator.delete
+      flash[:success] = "Collaborator removed from wiki."
+      redirect_to wiki_collaborators_path(@wiki)
     else
       flash[:error] = "There was an error deleting this collaborator. Please try again."
     end
